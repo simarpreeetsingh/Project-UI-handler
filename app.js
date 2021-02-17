7008dd5e8eca07880c7b093113d1048db1dc49e4
Simarpreet Singh <simarpreet@engagedly.com>
2021-02-18 00:09:58 +0530

    Code refactoring and validation changes.

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const app = require(`${__dirname}/router`)
 port = process.env.PORT || "7777"
 
 app.listen(port, () => {
-  console.log("*******************************")
+  console.log(`Listening on port ${port}`);
 })
--- task-app/views/signup.js
+++ task-app/views/signup.js
@@ -2,22 +2,24 @@ const Joi = require("joi")
 
 const signup = function (req, res) {
   const { error, value } = validator.validate(req.body)
-  if (error) {
-    console.log(error)
-    message = error.details[0].message
-    id = message.split(" ")[0]
-    locals = { id: id, message: message, values: error._original }
-    res.render("signup", locals)
-  }
+  if (error)
+    res.render("signup", errorBuilder(error))
 }
 
 const validator = Joi.object().keys({
   username: Joi.string().min(5).max(15).required(),
-  sex: Joi.string().required(),
+  sex: Joi.string().valid("male", "female", "other"),
   email: Joi.string().required(),
   password: Joi.string().min(5).max(20).required(),
   confirm_password: Joi.any().valid(Joi.ref('password')).required().options({}),
   dp: Joi.any()
 })
+
+const errorBuilder = function (error) {
+  message = error.details[0].message
+  id = message.split(" ")[0]
+  locals = { id: id, message: message, values: error._original }
+  return locals
+}
 
 module.exports = signup
